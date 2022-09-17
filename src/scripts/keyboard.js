8d39c2407726bb4ed21c2910661cafb7c35d2f81
jayfid <jayfid@protonmail.com>
2022-09-17 12:06:53 -0400

    change current context impl

--- src/scripts/keyboard.js
+++ src/scripts/keyboard.js
@@ -4,49 +4,39 @@ class KeyboardLayout {
   constructor(id, label) {
     this.id = id;
     this.label = label;
-    this.contexts = [];
+    this.contexts = {};
     this.currentContext = null;
   }
 
   // add/update context in lists of contexts
   addContext(id, characters, next) {
-    const context = { id, characters, next };
-
-    // if this context id is taken, update the existing entry
-    for (let i = 0; i < this.contexts.length; i += 1) {
-      if (this.contexts[i].id === id) {
-        this.contexts[i] = context;
-        return;
-      }
-    }
-    this.contexts.push(context);
+    const context = { characters, next };
+    this.contexts[id] = context;
     if (this.currentContext === null) {
-      this.currentContext = context;
+      this.currentContext = id;
     }
   }
 
   // return count of characters in current context
   getCharacterCount() {
     if (this.currentContext) {
-      return this.currentContext.characters.length;
+      return this.contexts[this.currentContext].characters.length
     }
     return false;
   }
 
   // return context with given id
   getContextById(id) {
-    for (let i = 0; i < this.contexts.length; i += 1) {
-      if (id === this.contexts[i].id) {
-        return this.contexts[i];
-      }
+    if (!this.contexts[id]) {
+      throw Error("Keyboard with supplied id does not exist");
     }
-    throw Error("Keyboard with supplied id does not exist");
+    return this.contexts[id];
   }
 
   // return value from character array at index
   getCharacterAtIndex(index) {
     if (index < this.getCharacterCount()) {
-      return this.currentContext.characters[index];
+      return this.contexts[this.currentContext].characters[index];
     }
     throw Error("Index out of bounds");
   }
@@ -54,25 +44,18 @@ class KeyboardLayout {
   // return next "random" character
   getNextCharacter() {
     return this.getCharacterAtIndex(
-      getRandomIntWithLimit(this.currentContext.characters.length)
+      getRandomIntWithLimit(this.getCharacterCount())
     );
   }
 
   // choose next context and replace current context with it
   nextContext() {
-    if (!this.currentContext.next || !this.currentContext.next.length) {
+    if (!this.contexts[this.currentContext].next || !this.contexts[this.currentContext].next.length) {
       throw Error("no available context");
     }
-    const nextIndex = getRandomIntWithLimit(this.currentContext.next.length);
-    const nextContextId = this.currentContext.next[nextIndex];
-    this.currentContext = this.getContextById(nextContextId);
-  }
-
-  // sets current context to first context in list
-  resetContext() {
-    if (this.contexts && this.contexts.length) {
-      [this.currentContext] = this.contexts;
-    }
+    const nextIndex = getRandomIntWithLimit(this.contexts[this.currentContext].next.length);
+    const nextContextId = this.contexts[this.currentContext].next[nextIndex];
+    this.currentContext = nextContextId;
   }
 }
 
